3f8eb5c2a51b3cbb99ec627fda6448c3d4ce0b02
RKabir <taposkabir@yahoo.com>
2023-05-05 05:15:34 +0600

    navbar completed

--- src/components/ButtonMain.js
+++ src/components/ButtonMain.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ButtonMain = ({children}) => {
   return (
-    <div className='bg-rose-600 px-9 py-4 inline-block border border-solid border-rose-700 hover:border-rose-100 
+    <div className='bg-rose-600 px-9 py-3 inline-block border border-solid border-rose-700 hover:border-rose-100 
         rounded text-white font-fontBwfc font-semibold 
-        hover:bg-transparent hover:text-rose-800 transition ease-in-out delay-150 shadow-xl'>
+        hover:bg-transparent hover:text-rose-800 transition ease-in-out delay-200 shadow-xl'>
         {children}
     </div>
   )
--- src/components/layouts/ButtonBase.js
+++ src/components/layouts/ButtonBase.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonBase = ({children}) => {
   return (
-    <div className='px-9 py-4 inline-block border border-solid border-rose-100 hover:border-rose-700
+    <div className='px-9 py-3 inline-block border border-solid border-rose-100 hover:border-rose-700
         rounded font-fontBwfc font-semibold bg-transparent hover:bg-rose-600 text-rose-800 hover:text-white
         transition ease-in-out delay-150 shadow-xl'>
           {children}
