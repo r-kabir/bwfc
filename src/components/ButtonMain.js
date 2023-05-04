42025945f29e7397ff790bfd2fbc7c1423afd202
RKabir <taposkabir@yahoo.com>
2023-05-05 03:31:42 +0600

    two button components created & navbar structure done

--- src/components/ButtonMain.js
+++ src/components/ButtonMain.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonMain = ({children}) => {
   return (
-    <div className='bg-rose-600 px-9 py-4 inline-block border border-solid border-rose-800 
+    <div className='bg-rose-600 px-9 py-4 inline-block border border-solid border-rose-700 hover:border-rose-100 
         rounded text-white font-fontBwfc font-semibold 
         hover:bg-transparent hover:text-rose-800 transition ease-in-out delay-150 shadow-xl'>
         {children}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from './layouts/Container'
+import ButtonMain from './ButtonMain'
+import ButtonBase from './layouts/ButtonBase'
+import axios from 'axios'
 
 const Navbar = () => {
+
+    let [navinfo, setNavinfo] = useState({})
+    let [loading, setLoading] = useState(true)
+
+    useEffect(()=> {
+        async function nav (){
+            let navbar = await axios.get("https://bwfc-api.vercel.app/navbar")
+            setNavinfo(navbar.data)
+            setLoading(false)
+        }
+        nav()
+    }, [])
+
+    if (loading){
+        return
+    }
+
   return (
     <Container>
         <div className='flex'>
-            <div className='w-1/5 bg-rose-200'><h1>logo</h1></div>
-            <div className='w-2/5 bg-lime-200'><h1>menu items</h1></div>
-            <div className='w-2/5 bg-yellow-200'><h1>nav buttons</h1></div>
+            <div className='w-1/5 bg-violet-300'><h1>logo</h1></div>
+            <div className='w-2/5 bg-lime-200'>
+                <ul className='flex'>{navinfo.navItems.map(anything =>(<li>{anything.item}</li>))}</ul>
+            </div>
+            <div className='w-2/5 bg-white'>
+                <ButtonBase><a href='#'>{navinfo.buttonOne.text}</a></ButtonBase>
+                <ButtonMain><a href='#'>{navinfo.buttonTwo.text}</a></ButtonMain>
+                <ButtonMain><a href='#'>{navinfo.buttonThree.text}</a></ButtonMain>
+            </div>
         </div>
     </Container>
   )
